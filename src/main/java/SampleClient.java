b70f470ee25781a0eae8c5472404445db7028ec3
david kachani <dkachani@pilotfishtechnology.com>
2021-04-11 11:10:41 -0400

    Initial commit with comments added to the extra code

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -21,31 +21,40 @@ public class SampleClient {
         client.registerInterceptor(new LoggingInterceptor(false));
 
         // Search for Patient resources
-        @SuppressWarnings("unused")
 		Bundle response = client
                 .search()
                 .forResource("Patient")
                 .where(Patient.FAMILY.matches().value("SMITH"))
                 .returnBundle(Bundle.class)
                 .execute();
+
+		// use the Bundle response to create list of FHIR Patient objects
         List<Patient> listPatients = new ArrayList<Patient>();
         response.getEntry().forEach(entry -> {
             listPatients.add((Patient) entry.getResource());
         });
+
+        // String manipulation for printing
         List<String> printPatients = new ArrayList<String>();
         for (Patient pat : listPatients) {
+            // Birth date printed in YYYY-MM-DD format
             DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
             String strBirthDate = "UNKNOWN";
             if (pat.getBirthDate() != null) {
                 strBirthDate = dateFormat.format(pat.getBirthDate());
             }
+            // given name replacing "," by -
             String givenName = pat.getName().get(0).getGiven().get(0).getValueAsString().replaceFirst("\",\"", "-");
+            // making sure first name always has a capital letter to start for sorting
             String strToAdd = givenName.replace(givenName.charAt(0), givenName.toUpperCase().substring(0, 1).charAt(0)) + " " + pat.getName().get(0).getFamily() + " " + strBirthDate;
+            // making sure no duplicates added to printing list
             if (!printPatients.contains(strToAdd)) {
                 printPatients.add(strToAdd);
             }
         }
+        // sorting
         printPatients.sort(Comparator.naturalOrder());
+        // printing
         printPatients.forEach(System.out::println);
 
     }
